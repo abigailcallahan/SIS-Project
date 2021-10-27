c43ecf4fc46db4dfab1099034ad3e54ba9983e49
Jackson D. Alexander <jackson.dalexander@mullen.pvt.k12.co.us>
2021-10-27 12:13:01 -0600

    Starting main menu

--- SIS/src/RunSIS.java
+++ SIS/src/RunSIS.java
@@ -1,19 +1,19 @@
+import java.util.Scanner;
+import java.io.File;
+import java.io.IOException;
 
 public class RunSIS
 	{
 
 		public static void main(String[] args)
 			{
-				System.out.println("Hello World");
-
-				
-				
-				
-				
-				
-				System.out.println("Abigail was here");
-				
-
+			Scanner userInt = new Scanner(System.in);
+			System.out.println("What would you like to do?");
+			System.out.println("1) add or delete a student");
+			System.out.println("2) change student grades/schedule");
+			System.out.println("3) sort students");
+			
+			
 			}
 
 	}
