fe6d3301096cf4231c5e62449d02cf2c94f54d56
jackson.dalexander <jackson.dalexander@MU-C1-10.mullen.pvt.k12.co.us>
2021-10-28 11:20:13 -0600

    comments

--- SIS/src/RunSIS.java
+++ SIS/src/RunSIS.java
@@ -1,11 +1,11 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 
 public class RunSIS
 	{
+	//arraylist and variables
 	static ArrayList<Student>TVShows = new ArrayList<Student>();
 	static String student;
 	
@@ -15,16 +15,19 @@ public class RunSIS
 			}
 		public static void fillArrayList() throws IOException
 		{
+			//filling arraylist
 			Scanner myFile = new Scanner(new File("StudentList.txt"));
 			while (myFile.hasNext()) {	
 				student = myFile.nextLine();
 		}
-			
+			//printing out main menu
 			Scanner userInt = new Scanner(System.in);
 			System.out.println("What would you like to do?");
 			System.out.println("1) add or delete a student");
 			System.out.println("2) change student grades/schedule");
 			System.out.println("3) sort students");
+			
+			
 			
 			
 
