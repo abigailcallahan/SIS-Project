5ab55a67b02d7dc4f63989d3b8aafb2fe81d7ece
jackson.dalexander <jackson.dalexander@MU-C1-10.mullen.pvt.k12.co.us>
2021-10-28 11:10:46 -0600

    array list

--- SIS/src/RunSIS.java
+++ SIS/src/RunSIS.java
@@ -4,10 +4,15 @@ import java.io.IOException;
 
 public class RunSIS
 	{
-
-		public static void main(String[] args)
+	static String[] studentList = new String[24];
+	static Scanner myFile = new Scanner(new File("StudentList.txt"));
+	static String student;
+		public static void main(String[] args) throws IOException
 			{
-
+			while (myFile.hasNext()) {	
+				student = myFile.nextLine();
+			}
+			
 			Scanner userInt = new Scanner(System.in);
 			System.out.println("What would you like to do?");
 			System.out.println("1) add or delete a student");
