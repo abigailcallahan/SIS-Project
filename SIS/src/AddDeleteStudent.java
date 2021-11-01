1dc557b67031120168c44010978e28ad481edaff
Administrator <Administrator@R90PKYBQ>
2021-11-01 18:01:23 -0500

    put into methods

--- SIS/src/AddDeleteStudent.java
+++ SIS/src/AddDeleteStudent.java
@@ -17,155 +17,155 @@ public class AddDeleteStudent
 		static String firstG;
 		static String secondG;
 		static String thirdG;
-
 		
 		public static void main(String[] args) throws IOException
 			{
-				System.out.println("Would you like to: ");
-				System.out.println("1) Add a Student");
-				System.out.println("2) Delete a Student");
-				System.out.println("3) Go back to Main Menu");
-				answer = intInput.nextInt();
-				if(answer == 1)
-					{
-						//USE METHOD 1 HERE
-						System.out.println("What is the first name of your new student");
-						first = stringInput.nextLine();
-						//set name into array list
-//						RunSIS.new Student(setFirstName(first), 0, 0, 0, 0, 0, 0, 0);
-						//RunSIS.Student.setFirstName(first);
-						System.out.println("Last Name?");
-						last = stringInput.nextLine();
-						//last name into arraylist
-//						new Student = (first, setLastName(last), 0, 0, 0, 0, 0, 0);
-						System.out.println();
-						
-						
-						System.out.println("What is " + first + " " + last + "'s first period class? Your options are: ");
-						System.out.println("\t1) Biology\n\t2) English\n\t3) Algebra");
-						firstP = intInput.nextInt();
-						
-						if(firstP == 1)
-							{
-								//fill array
-								
-								System.out.println("What is their grade in Biology?");
-								System.out.println("\tuse a capitalized letter with a + or - or nothing");
-								firstG = stringInput.nextLine();
-								//fill array
-								
-								System.out.println("What about second period class?");
-								System.out.println("\t1) English\n\t2) Algebra");
-								secondP = intInput.nextInt();
-								if(secondP == 1)
-									{
-										//set 1 to class
-										//use setter 
-										//fill array
-										System.out.println("What is their grade in English?");
-										System.out.println("\tuse a capitalized letter with a + or - or nothing");
-										secondG = stringInput.nextLine();
-										//fill array with grade
-										//fill array with automatic third period
-										System.out.println("What is their grade in Algebra?");
-										System.out.println("\tuse a capitalized letter with a + or - or nothing");
-										thirdG = stringInput.nextLine();
-										//display all classes
-									}
-								else if(secondP == 2)
-									{
-										//set 2 to class
-										//fill array
-										System.out.println("What is their grade in Algebra?");
-										System.out.println("\tuse a capitalized letter with a + or - or nothing");
-										secondG = stringInput.nextLine();
-										//fill array with grade
-										//fill array with automatic third period
-										System.out.println("What is their grade in English?");
-										System.out.println("\tuse a capitalized letter with a + or - or nothing");
-										thirdG = stringInput.nextLine();
-										//display all classes
-									}
-								
-							}
-	
-						else if(firstP == 2)
-							{
-								//fill array
-								System.out.println("What about second period?");
-								System.out.println("\t1) Biology\n\t2) Algebra");
-								secondP = intInput.nextInt();
-								if(secondP == 1)
-									{
-										//fill array
-										//fill array with automatic third period
-										//display all classes
-									}
-								else if(secondP == 2)
-									{
-										//fill array
-										//fill array with automatic third period
-										//display all classes
-									}
-							}
-						
-						else if(firstP == 3)
-							{
-								//fill array
-								System.out.println("What about second period?");
-								System.out.println("\t1) Biology\n\t2) English");
-								secondP = intInput.nextInt();
-								if(secondP == 1)
-									{
-										//fill array
-										
-										//fill array with automatic third period
-										//display all classes
-									}
-								else if(secondP == 2)
-									{
-										//fill array
-										//fill array with automatic third period
-										//display all classes
-									}
-							}
-						else
-							{
-								System.out.println("You did not enter a valid answer, you will restart.");
-							}
-						
-					}
-				
-				//END METHOD 1 HERE
-				
-				else if(answer == 2)
-					{
-						//START METHOD 2 HERE
-						//display roster
-						//for(int i = 0; i < RunSIS.TVShows.size(); i++)
-							{
-								System.out.println("Test");
-								//System.out.println(i + 1 + ") ");
-								//System.out.print(RunSIS.TVShows.get(i));
-							}
-							//END METHOD 2 HERE
-					}
-				
-				else if(answer == 3)
-					{
-						//go to menu
-					}
-
-				//IF THEY PICK ADD
-					//get new name
-					//get new GPA
-					//figure out the order of periods
-				//IF THEY PICK DELETE
-					//display the students
-					//ask which student they would like to delete
-					//delete name based on input
-					//display new names
-
+				for(int i = 0; i < RunSIS.StudentList.size(); i++)
+				{
+					System.out.println(i); 
+				}
+//				System.out.println("Would you like to: ");
+//				System.out.println("1) Add a Student");
+//				System.out.println("2) Delete a Student");
+//				System.out.println("3) Go back to Main Menu");
+//				answer = intInput.nextInt();
+//				
+//				if(answer == 1)
+//					{
+//						addStudent();
+//					}
+//				else if(answer == 2)
+//					{
+//						deleteStudent();
+//						
+//					}
+//				
+//				else if(answer == 3)
+//					{
+//						//go to menu
+//					}
 			}
+		
+		public static void addStudent()
+		{
+			System.out.println("What is the first name of your new student");
+			first = stringInput.nextLine();
+			//set name into array list
+//			RunSIS.new Student(setFirstName(first), 0, 0, 0, 0, 0, 0, 0);
+			//RunSIS.Student.setFirstName(first);
+			System.out.println("Last Name?");
+			last = stringInput.nextLine();
+			//last name into arraylist
+//			new Student = (first, setLastName(last), 0, 0, 0, 0, 0, 0);
+			System.out.println();
+			
+			
+			System.out.println("What is " + first + " " + last + "'s first period class? Your options are: ");
+			System.out.println("\t1) Biology\n\t2) English\n\t3) Algebra");
+			firstP = intInput.nextInt();
+			
+			if(firstP == 1)
+				{
+					//fill array
+					
+					System.out.println("What is their grade in Biology?");
+					System.out.println("\tuse a capitalized letter with a + or - or nothing");
+					firstG = stringInput.nextLine();
+					//fill array
+					
+					System.out.println("What about second period class?");
+					System.out.println("\t1) English\n\t2) Algebra");
+					secondP = intInput.nextInt();
+					if(secondP == 1)
+						{
+							//set 1 to class
+							//use setter 
+							//fill array
+							System.out.println("What is their grade in English?");
+							System.out.println("\tuse a capitalized letter with a + or - or nothing");
+							secondG = stringInput.nextLine();
+							//fill array with grade
+							//fill array with automatic third period
+							System.out.println("What is their grade in Algebra?");
+							System.out.println("\tuse a capitalized letter with a + or - or nothing");
+							thirdG = stringInput.nextLine();
+							//display all classes
+						}
+					else if(secondP == 2)
+						{
+							//set 2 to class
+							//fill array
+							System.out.println("What is their grade in Algebra?");
+							System.out.println("\tuse a capitalized letter with a + or - or nothing");
+							secondG = stringInput.nextLine();
+							//fill array with grade
+							//fill array with automatic third period
+							System.out.println("What is their grade in English?");
+							System.out.println("\tuse a capitalized letter with a + or - or nothing");
+							thirdG = stringInput.nextLine();
+							//display all classes
+						}
+					
+				}
 
+			else if(firstP == 2)
+				{
+					//fill array
+					System.out.println("What about second period?");
+					System.out.println("\t1) Biology\n\t2) Algebra");
+					secondP = intInput.nextInt();
+					if(secondP == 1)
+						{
+							//fill array
+							//fill array with automatic third period
+							//display all classes
+						}
+					else if(secondP == 2)
+						{
+							//fill array
+							//fill array with automatic third period
+							//display all classes
+						}
+				}
+			
+			else if(firstP == 3)
+				{
+					//fill array
+					System.out.println("What about second period?");
+					System.out.println("\t1) Biology\n\t2) English");
+					secondP = intInput.nextInt();
+					if(secondP == 1)
+						{
+							//fill array
+							
+							//fill array with automatic third period
+							//display all classes
+						}
+					else if(secondP == 2)
+						{
+							//fill array
+							//fill array with automatic third period
+							//display all classes
+						}
+				}
+			else
+				{
+					System.out.println("You did not enter a valid answer, you will restart.");
+				}
+			
+		}
+		public static void deleteStudent()
+		{
+			//display roster
+			//for(int i = 0; i < RunSIS.TVShows.size(); i++)
+				{
+					System.out.println("Test");
+					//System.out.println(i + 1 + ") ");
+					//System.out.print(RunSIS.TVShows.get(i));
+				}
+		}
+	
+	
+	
+	
 	}
